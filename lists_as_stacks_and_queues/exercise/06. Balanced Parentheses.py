31efab685ea93503246ce76bde4663bf4761c7f3
BobsunPower <bobsunpower@gmail.com>
2022-02-22 20:54:53 +0200

    Completed

--- lists_as_stacks_and_queues/exercise/06. Balanced Parentheses.py
+++ lists_as_stacks_and_queues/exercise/06. Balanced Parentheses.py
@@ -1,9 +1,5 @@
-def check(par):
-    bks = ['()', '{}', '[]']
-    while any(x in par for x in bks):
-        for b in bks:
-            par = par.replace(b, '')
-    return not par
-
-
-print("YES" if check(input()) else "NO")
+par, bks = input(), ['()', '{}', '[]']
+while any(x in par for x in bks):
+    for b in bks:
+        par = par.replace(b, '')
+print("NO" if par else "YES")
