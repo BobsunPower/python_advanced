df3e613288117394b6d3294d2a8d45284cf14eed
BobsunPower <bobsunpower@gmail.com>
2022-02-27 10:57:33 +0200

    Completed

---
+++ tuples_and_sets/exercise/06. Battle of Names.py
@@ -0,0 +1,9 @@
+lst, tot, evn, odd = [], 0 , set(), set()
+for i in range(1, int(input()) + 1):
+    lst = [(sum(ord(letter) for letter in input()))]
+    res = sum(lst) // i
+    lst.clear()
+    evn.add(res) if res % 2 == 0 else odd.add(res)
+print(", ".join(str(x) for x in (evn | odd))) if sum(evn) > sum(odd) else None
+print(", ".join(str(x) for x in (odd - evn))) if sum(evn) < sum(odd) else None
+print(", ".join(str(x) for x in (odd | evn))) if sum(evn) == sum(odd) else None
