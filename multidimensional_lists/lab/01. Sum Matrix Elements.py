a2ec8b4660dff7f625d060f9f0d6467c5ba4579b
BobsunPower <bobsunpower@gmail.com>
2022-02-28 14:29:17 +0200

    Completed

--- multidimensional_lists/lab/01. Sum Matrix Elements.py
+++ multidimensional_lists/lab/01. Sum Matrix Elements.py
@@ -1,3 +1,3 @@
 row, _ = map(int, input().split(', '))
 mtx = [[int(n) for n in input().split(', ')] for _ in range(row)]
-print(sum([sum(lst) for lst in mtx]), '\n', mtx)
+print(sum([sum(r) for r in mtx]), '\n', mtx)
