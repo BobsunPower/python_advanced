cd2062720b0b07deb2920484639f9081cea043a5
BobsunPower <bobsunpower@gmail.com>
2022-03-10 15:25:54 +0200

    Completed

--- multidimensional_lists/exercise/07. Snake Moves.py
+++ multidimensional_lists/exercise/07. Snake Moves.py
@@ -1,18 +1,14 @@
-# TODO
 from collections import deque
 row, col = [int(x) for x in input().split()]
-deq = deque(list(input()))
-mtx = []
+deq, mtx = deque(input()), []
 for i in range(row):
     mtx.append([])
     for j in range(col):
         mtx[i].append("")
 for i in range(row):
     for j in range(col):
-        cnt_col = j
-        cnt_chr = deq.popleft()
-        if i % 2 != 0:
-            cnt_col = col - 1 - j
+        cnt_col, cnt_chr = j, deq.popleft()
+        cnt_col = col - 1 - j if i % 2 != 0 else cnt_col
         mtx[i][cnt_col] = cnt_chr
         deq.append(cnt_chr)
-[print("".join(str(x) for x in row)) for row in mtx]
+[print("".join(str(i) for i in row)) for row in mtx]
