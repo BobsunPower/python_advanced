d5df877ea09b21b16c92578b8bd9b0c087902103
BobsunPower <bobsunpower@gmail.com>
2022-03-11 12:51:50 +0200

    Completed

--- functions_advanced/lab/03. Cheese Showcase.py
+++ functions_advanced/lab/03. Cheese Showcase.py
@@ -1,1 +1,5 @@
-# TODO
+def sorting_cheeses(**kwa):
+    dic, out = {k: v for k, v in sorted(kwa.items(), key=lambda i: len(i[1])).__reversed__()}, ''
+    for k, v in dic.items():
+        out += f'\n{k}\n' + '\n'.join([str(i) for i in sorted(v).__reversed__()])
+    return out
