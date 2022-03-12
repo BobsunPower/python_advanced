4303b31b9ee5cd3c4b4ac20a524ac05c3e7947ec
BobsunPower <bobsunpower@gmail.com>
2022-03-12 15:48:00 +0200

    Completed

--- functions_advanced/exercise/01. Negative vs Positive.py
+++ functions_advanced/exercise/01. Negative vs Positive.py
@@ -1,1 +1,4 @@
-# TODO
+lst, pos, neg = list(map(int, input().split())), [], []
+[pos.append(n) if n > 0 else neg.append(n) for n in lst]
+mor, les = ['negatives', 'positives'] if abs(sum(neg)) > sum(pos) else ['positives', 'negatives']
+print(f"{sum(neg)}\n{sum(pos)}\nThe {mor} are stronger than the {les}")
