1b1f5b5fa45cdab07f07c0c9061964f71f09e19c
BobsunPower <bobsunpower@gmail.com>
2022-03-12 15:50:12 +0200

    Completed

---
+++ functions_advanced/exercise/06. Function Executor.py
@@ -0,0 +1,13 @@
+def func_executor(*arg):
+    return [f(*n) for f, n in arg]
+
+
+def sum_numbers(num1, num2):
+    return num1 + num2
+
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+
+print(func_executor((sum_numbers, (1, 2)), (multiply_numbers, (2, 4))))
